b28a7b44a304800577c17a161e90bd18fca752fa
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-04-21 12:05:44 +0300

    AbstractFactory, prorype

---
+++ 05_AbstractFactory/05_AbstractFactory.cpp
@@ -0,0 +1,153 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+class Toy
+{
+protected:
+	string name;
+
+public:
+	Toy()
+	{
+			
+	}
+	Toy(string _name)
+	{
+		name = _name;
+	}
+	virtual void ShowInfo() const
+	{
+		cout << "I`m a toy, name: " << name << endl;
+	}
+};
+
+class Cat : public Toy
+{
+public:
+	Cat(string name) : Toy(name)//делегування
+	{
+	}
+	virtual void ShowInfo() const override
+	{
+		cout << "I`m a cat, name: " << name << endl;
+	}
+};
+class Bear : public Toy
+{
+public:
+	Bear(string name) : Toy(name)
+	{
+	}
+	virtual void ShowInfo() const override
+	{
+		cout << "I`m a bear, name: " << name << endl;
+	}
+};
+
+class TeddyBear : public Bear
+{
+public:
+	TeddyBear() : Bear("TED")
+	{
+	}
+	virtual void ShowInfo() const override
+	{
+		cout << "I`m a teddy bear, name: " << name << endl;
+	}
+};
+class TeddyCat : public Cat
+{
+public:
+	TeddyCat() : Cat("TOM")
+	{
+	}
+	virtual void ShowInfo() const override
+	{
+		cout << "I`m a teddy cat, name: " << name << endl;
+	}
+};
+
+class WoodenBear : public Bear
+{
+public:
+	WoodenBear() : Bear("GRIZLI")
+	{
+	}
+	virtual void ShowInfo() const override
+	{
+		cout << "I`m a wooden bear, name: " << name << endl;
+	}
+};
+class WoodenCat : public Cat
+{
+public:
+	WoodenCat() : Cat("MURKAS")
+	{
+	}
+	virtual void ShowInfo() const
+	{
+		cout << "I`m a wooden cat, name: " << name << endl;
+	}
+};
+
+class ToyFactory
+{
+public:
+	virtual Bear* GetBear() = 0;
+	virtual Cat* GetCat() = 0;
+};
+
+class TeddyToyFactory : public ToyFactory
+{
+public:
+	Bear* GetBear() override
+	{
+		/*TeddyBear bear =  TeddyBear();
+		TeddyBear* pbear = &bear;
+		return pbear;*/
+		return new TeddyBear();
+	}
+	Cat* GetCat() override
+	{
+		return new TeddyCat();
+	}
+};
+
+class WoodenToyFactory : public ToyFactory
+{
+public:
+	Bear* GetBear() override
+	{
+		return new WoodenBear();
+	}
+	Cat* GetCat() override
+	{
+		return new WoodenCat();
+	}
+};
+
+void main()
+{
+	Toy t("Hourse");
+
+	Toy d(t);//ctor copy
+	Toy s;//cstor copy
+	s = t;//operator =
+	ToyFactory* factory =  new WoodenToyFactory();//new TeddyToyFactory();
+
+	//Bear *b =  factory->GetBear();
+	//b->ShowInfo();
+	Bear* bear = factory->GetBear();
+	Cat* cat = factory->GetCat();
+
+	bear->ShowInfo();
+	cat->ShowInfo();
+
+	delete cat;
+	delete bear;
+	//delete b;
+	delete factory;
+
+	system("pause");
+}
---
+++ 06_Prototype/06_Prototype.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// Prototype Interface
+template<class T>
+class Prototype
+{
+public:
+	virtual T Clone() const = 0;
+};
+
+struct Date
+{
+	int year, month, day;
+
+	string ToString() const
+	{
+		return to_string(day) + "/" + to_string(month) + "/" + to_string(year);
+	}
+};
+
+class Member : public Prototype<Member>
+{
+private:
+	string name;
+	int age;
+public:
+	
+	Member() : name("no name"), age(0) { }
+	Member(string name, int age) : name(name), age(age) {  }
+
+	Member Clone() const
+	{
+		return *this;//00xgh562ff
+	}
+
+	string ToString() const
+	{
+		return name + to_string(age);
+	}
+};
+
+class Event : public Prototype<Event>
+{
+private:
+	string name;//Independency day    Independency day
+	Date date;//24/08/2023            24/08/2023
+	string address;//Soborna          Soborna
+	Member* members;//0x00ghtr56      0x000123
+	int membersCount;//10             10
+public:  
+
+	Event() : name("no name"), date(Date{ 0, 0, 0 }), address("no address") { }
+	Event(string name, Date date, string address) : name(name), date(date), address(address) { }
+
+	void AddTestMembers()
+	{
+		membersCount = 2;
+		members = new Member[membersCount];
+
+		members[0] = Member("Bob", 30);
+		members[1] = Member("Vova", 44);
+	}
+	Event Clone() const override
+	{
+		Event clone = *this;//поверхнева копія
+		clone.members = new Member[membersCount];//глибоке копіювання
+		for (int i = 0; i < membersCount; i++)
+		{
+			clone.members[i] = this->members[i].Clone();
+		}
+		return clone;
+	}
+	/*~Event()
+	{
+		if (members != nullptr)
+			delete[]members;
+	}*/
+
+	string ToString() const
+	{
+		string res = name + " : " + date.ToString() + "\n" + address;
+		for (int i = 0; i < membersCount; i++)
+		{
+			res += "\n\tMember[" + to_string(i + 1) + "] - " + members[i].ToString();
+		}
+		return res;
+	}
+
+
+};
+
+
+void main()
+{
+	
+
+	// testing...
+	Event event1("Independence Day", Date{ 2023, 8, 24 }, "Kyiv, Ukraine");
+	// initializing...
+	event1.AddTestMembers();
+
+	cout << event1.ToString() << endl;
+
+	Event cloneEvent = event1.Clone();
+
+	cout << cloneEvent.ToString() << endl;
+}
+
+
+
+
+
+
+
+
+//int sum(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//constexpr int new_sum(int a, int b)
+//{
+//	return a + b;
+//}
+
+//void func()
+//{
+//	int a, b;
+//	cin >>a>>b;
+//	constexpr int a1 = new_sum(5, 12); // ОК: constexpr-переменная
+//	const int a2 = new_sum(5, 12); // ОК: constexpr-переменная
+//	//constexpr int a1 = new_sum(a, b); // ОК: constexpr-переменная
+//	//constexpr int a2 = sum(5, 12); // ошибка: функция sum не является constexp-выражением
+//	int a3 = new_sum(5, 12); // ОК: функция будет вызвана на этапе компиляции
+//	int a4 = sum(5, 12); // ОК
+//}
